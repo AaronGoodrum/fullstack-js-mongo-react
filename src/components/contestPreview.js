0a3f3b662292a6cd6b8c34093af1b09c5adb4ad1
Aaron G <ag505181@gmail.com>
2019-03-13 08:44:05 -0700

    update webpack missing plugin transform stage 2

--- src/components/contestPreview.js
+++ src/components/contestPreview.js
@@ -1,29 +1,27 @@
-import React from 'react'
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default class contestPreview extends Component {
+class ContestPreview extends Component {
+  handleClick = () => {
+    console.log(this.props.username)
+  }
   render() {
     return (
-      <div className="ContestPreview" onClick={...}>
+      <div className="ContestPreview" onClick={this.handleClick}>
       <div className="company-name">
-        {userData.company.name}
+        {this.props.company.name}
       </div>
       <div className="user-name">
-        {userData.name}
+        {this.props.username}
       </div>
     </div>
     )
   }
 }
 
-const ContestPreview = (userData) => (
-  <div className="ContestPreview" onClick={...}>
-    <div className="company-name">
-      {userData.company.name}
-    </div>
-    <div className="user-name">
-      {userData.name}
-    </div>
-  </div>
-)
+ContestPreview.protoTypes = {
+  username: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+
 export default ContestPreview
